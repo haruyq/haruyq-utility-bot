3b59ee672c2e5169219ea1365ede152ac6d693fe
haruyq <156767674+haruyq@users.noreply.github.com>
2026-01-07 13:48:43 +0900

    Implement the addition of AnswerModal and display a captcha image on the verification button of VerifyView

--- views/verify.py
+++ views/verify.py
@@ -1,13 +1,21 @@
 import discord
 
-from mod.db import VerifyDB
+from typing import Any
 
-class VerifyView(discord.ui.View):
+from mod.db import VerifyDB
+from mod.gen_cap import generate_captcha
+
+class AnswerModal(discord.ui.Modal, title="画像の数字を入力して下さい"):
     def __init__(self):
-        super().__init__(timeout=None)
-        
-    @discord.ui.button(label="✅｜認証する", style=discord.ButtonStyle.blurple)
-    async def verify_btn(self, interaction: discord.Interaction, _button: discord.ui.Button["VerifyView"]) -> None:
+        super().__init__()
+        self.answer: discord.ui.TextInput[Any] = discord.ui.TextInput(
+            label="画像の数字を入力してください",
+            placeholder="12345",
+            max_length=5,
+        )
+        self.add_item(self.answer)
+    
+    async def on_submit(self, interaction: discord.Interaction) -> None:
         if not interaction.guild:
             return
         
@@ -34,4 +42,27 @@ class VerifyView(discord.ui.View):
             return
         
         await member.add_roles(role, reason="User verified via verification panel.")
-        await interaction.response.send_message("認証が完了しました！", ephemeral=True)
+        await interaction.response.send_message("認証が完了しました。", ephemeral=True)
+
+class VerifyView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        
+    @discord.ui.button(label="✅｜認証する", style=discord.ButtonStyle.blurple)
+    async def verify_btn(self, interaction: discord.Interaction, _button: discord.ui.Button["VerifyView"]) -> None:
+        if not interaction.guild:
+            return
+        
+        img_data = generate_captcha()
+        file = discord.File(fp=img_data, filename="captcha.png")
+        
+        embed = discord.Embed(
+            description="以下の画像に表示されている数字を入力してください。",
+            color=discord.Colour.blue(),
+        )
+        embed.set_image(url="attachment://captcha.png")
+        await interaction.response.send_message(embed=embed, file=file, ephemeral=True)
+    
+    @discord.ui.button(label="答えを入力する", style=discord.ButtonStyle.gray)
+    async def answer_btn(self, interaction: discord.Interaction, _button: discord.ui.Button["VerifyView"]) -> None:
+        await interaction.response.send_modal(AnswerModal())
