068d3606dc58833885b3bc7911b0cc4ddc254420
haruyq <156767674+haruyq@users.noreply.github.com>
2026-01-08 21:05:37 +0900

    Fix BuildEmbedModal to handle empty title and description inputs

--- views/embed.py
+++ views/embed.py
@@ -9,22 +9,38 @@ class BuildEmbedModal(discord.ui.Modal, title="埋め込みメッセージの作
             label="タイトル",
             placeholder="埋め込みメッセージのタイトルを入力してください",
             max_length=256,
+            required=False,
         )
         self.description_input: discord.ui.TextInput[Any] = discord.ui.TextInput(
             label="説明",
             style=discord.TextStyle.paragraph,
             placeholder="埋め込みメッセージの説明を入力してください",
             max_length=2048,
+            required=False,
         )
         self.add_item(self.title_input)
         self.add_item(self.description_input)
 
     async def on_submit(self, interaction: discord.Interaction) -> None:
+        if not interaction.channel:
+            return
+        
+        title = self.title_input.value or None
+        description = self.description_input.value or None
+        
+        if not title and not description:
+            await interaction.response.send_message("タイトルまたは説明のいずれかを入力してください。", ephemeral=True)
+            return
+        
         embed = discord.Embed(
-            title=self.title_input.value,
-            description=self.description_input.value,
+            title=title,
+            description=description,
             color=discord.Colour.blue(),
         )
         
+        msg = None
         if isinstance(interaction.channel, discord.TextChannel):
-            await interaction.channel.send(embed=embed)
+            msg = await interaction.channel.send(embed=embed)
+        
+        if msg:
+            await interaction.response.send_message(f"埋め込みメッセージを送信しました -> {msg.jump_url}", ephemeral=True)
