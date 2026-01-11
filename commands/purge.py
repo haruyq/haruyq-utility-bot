d9cb73a4ba0c2716771368c2a43cfea2f869dddb
haruyq <156767674+haruyq@users.noreply.github.com>
2026-01-11 23:22:00 +0900

    Add purge command

---
+++ commands/purge.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+from typing import Optional
+
+from mod.logger import Logger
+
+Log = Logger(__name__)
+
+class PurgeCommand(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        
+    def _check(self, msg: discord.Message, user: discord.User) -> bool:
+        return msg.author.id == user.id
+    
+    @app_commands.command(name="purge", description="メッセージを一括削除します。")
+    @app_commands.checks.has_permissions(administrator=True)
+    async def purge(self, interaction: discord.Interaction, amount: int, user: Optional[discord.User]) -> None:
+        if not interaction.guild:
+            return
+        
+        await interaction.response.defer()
+        
+        channel = interaction.channel
+        if not isinstance(channel, (discord.TextChannel, discord.VoiceChannel, discord.Thread)):
+            await interaction.response.send_message("このコマンドはテキストチャンネルでのみ使用できます。", ephemeral=True)
+            return
+        
+        try:
+            if user:
+                await channel.purge(limit=amount, check=lambda msg: self._check(msg, user))
+            
+            else:
+                await channel.purge(limit=amount)
+            
+        except Exception as e:
+            Log.error(f"Purge Failed: {e}", exc_info=True)
+            await interaction.followup.send("エラーが発生しました。")
+            return
+        
+        embed = discord.Embed(
+            description=f"{amount} 件のメッセージが削除されました。",
+            color=discord.Colour.green()
+        )
+        await interaction.followup.send(embed=embed)
